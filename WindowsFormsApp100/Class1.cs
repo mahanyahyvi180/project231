be63fd4bacaafa69d672c64136e65931e77a283d
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-27 10:36:59 +0330

    Add files via upload

---
+++ WindowsFormsApp100/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp100
+{
+    class cEployee : IEmployee
+    {
+       public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Concatinate()
+        {
+            return FirstName + ' ' + LastName;
+
+        }
+    }
+}
